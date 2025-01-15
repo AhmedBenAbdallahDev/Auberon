5b0975314c52e7225df176dcf6e87a0c993665a6
Mohamed Ashref Ben Abdallah <109633107+Ashref-dev@users.noreply.github.com>
2025-01-15 04:24:33 +0100

    W

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,7 +18,7 @@ const Index = () => {
             <div className="col-lg-8">
               <div className="hero-content wow fadeInLeft delay-0-2s">
                 <h1>
-                  Web <span>Auberon</span> <i>Agency</i>
+                  Auberon<span>Design</span> <i>Agency</i>
                 </h1>
               </div>
             </div>
